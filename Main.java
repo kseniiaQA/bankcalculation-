35b829184f432752f21dad1238db346df658fd81
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-05-11 11:34:42 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int account = 2000000000;
-        int withdrawal = 500000000;
-        int total = account - withdrawal;
-        System.out.println(total);
-    }
-
-}
